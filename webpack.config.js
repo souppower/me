fb7b6c7666e18e1933560b3b912e7590a89cdac6
souppower <souppower@goodpatch.com>
2017-04-19 00:26:37 +0900

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,44 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
-	entry: "./js/main.js",
-	output: {filename: "./dist/bundle.js"},
+	entry: "./src/js/index.js",
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: "bundle.js",
+		publicPath: 'dist/'
+	},
 	module: {
 		rules: [
-		{
-			test: /\.js$/,
-			exclude: /node_modules/,
-			loader: "babel-loader",
-			query: {
-				presets: ['es2015'],
-				plugins: ['transform-runtime']
-			}
-		},
-		{
-			test: /\.scss$/,
-			use: [
-				{
-					loader: "sass-loader"
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: "babel-loader",
+				query: {
+					presets: ['es2015'],
+					plugins: ['transform-runtime']
 				}
-			]
-		}
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					'style-loader',
+					{
+						loader: 'css-loader',
+						options: {
+							importLoaders: 1,
+						}
+					},
+                    {
+						loader: 'sass-loader',
+						options: {
+							resources: [
+								'./src/sass/libs'
+							]
+						}
+					}
+				]
+			},
 		]
 	}
 };
